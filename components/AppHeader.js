451cd023594e6e4ce5d0efe1156b2c7f52552fc9
PavithraWHJRTeacher <pavithra.i@byjusteachers.com>
2022-03-05 00:02:19 +0530

    Commiting Team-Voting

--- components/AppHeader.js
+++ components/AppHeader.js
@@ -13,7 +13,7 @@ export default class AppHeader extends React.Component{
 
 const styles = StyleSheet.create({
   textContainer:{
-    backgroundColor: '#FFC300'
+    backgroundColor: 'green'
   },
   text:{
     padding: 20,
